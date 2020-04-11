c19a7917219b88783b03778d816c7174e8d19d26
Shadab-ZED24 <shadabk128@gmail.com>
2020-04-11 13:27:55 +0530

    server.js commit

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3002, function () {
-  console.log('Example app listening on port 3002!');
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
 
 app.get('/', function (req, res) {
